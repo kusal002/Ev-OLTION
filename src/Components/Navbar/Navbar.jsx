ce6d74b6b2d96173095c3b2855b3530807883137
kusal002 <systematicshift002@gmail.com>
2024-06-19 12:00:26 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="nav-logo">EV-oLution</div>
+      <ul className="nav-menu">
+        <li className="home">Home</li>
+        <li className="explore">Explore</li>
+        <li className="about">About</li>
+        <li className="nav-contact">Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
